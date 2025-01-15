29a7667b98336e05758911449533d994a59f11c2
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2025-01-16 00:25:48 +0200

    fix spelling

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,4 +1,4 @@
-import Button from "../components/layouts/Button";
+import Button from "../components/layout/Button";
 import { useRouter } from "next/router";
 import { PageRouteProps } from "@/interface";
 
