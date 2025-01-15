353dd2fe4c42b9df43896d0841cf4a4a53395e23
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2025-01-16 00:51:26 +0200

    check

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,4 +1,4 @@
-import Button from "../components/layout/Button";
+import Button from "../components/common/Button";
 import { useRouter } from "next/router";
 import { PageRouteProps } from "@/interface";
 
